19c31de6b0fccc40cbef743fa0f4c73025367e29
misha <prs778888@gmail.com>
2024-02-29 23:01:41 +0600

    test

---
+++ src/com/Test/Main.java
@@ -0,0 +1,16 @@
+package com.Test;
+import java.sql.SQLOutput;
+import  java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+
+//        System.out.print("Введите ваше имя: ");
+//        String username = scan.nextLine();
+//        System.out.println("Привет, " + username);
+        }
+}
